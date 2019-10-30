c2fb3e266f513c5797403a7092b39afc420bcefe
liushuyu <liushuyu011@gmail.com>
2019-10-29 20:13:18 -0600

    ci: automatically builds Docker image

---
+++ .ci/build-latest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+function find_latest() {
+    VARIANT_FOLDER="$2"
+    if [[ "$1" == arm* && "$2" == 'base' ]]; then
+        VARIANT_FOLDER='generic'
+    fi
+
+    cat << 'EOF' > t.pl
+use strict;
+my $regex = qr/aosc-os_%var%_\d{8}(?>_amd64)?\.tar\.(?>gz|xz)/mp;
+my @matches = <STDIN> =~ /$regex/g;
+foreach my $match (@matches) {print "$match\n"};
+EOF
+
+    sed -i "s/%var%/$2/g" t.pl
+
+    TARBALL_NAME=$(curl -s "https://releases.aosc.io/os-$1/${VARIANT_FOLDER}/" | perl -n t.pl | sort | tail -n1)
+    if [[ "x${TARBALL_NAME}" == 'x' ]]; then
+    echo 'Cannot find latest tarball'
+    exit 1
+    fi
+
+    export TARBALL_URL="https://repo.aosc.io/aosc-os/os-$1/${VARIANT_FOLDER}/${TARBALL_NAME}"
+    rm -f t.pl
+}
+
+COMPONENT="${2/aosc-os/}"
+COMPONENT="${COMPONENT#"-"}"
+find_latest "$1" "$COMPONENT"
+./scriptlets/gen_dockerfile.sh
--- scriptlets/gen_dockerfile.sh
+++ scriptlets/gen_dockerfile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-read -r -p "[*] Enter tarball download URL: " TARBALL_URL
-TARBALL_NAME="$(basename "${TARBALL_URL}")"
+if [[ "x${TARBALL_URL}" == "x" ]]; then
+  read -r -p "[*] Enter tarball download URL: " TARBALL_URL
+  TARBALL_NAME="$(basename "${TARBALL_URL}")"
+fi
 echo "[+] Downloading tarball..."
 wget -c "${TARBALL_URL}"
 if ! test -e "${TARBALL_NAME}"; then
@@ -8,16 +10,17 @@ if ! test -e "${TARBALL_NAME}"; then
     exit 127
 fi
 IMG_VER=$(echo "${TARBALL_NAME}" | perl -nle '/^aosc-.*_(\d+)_.*$/; print $1')
-if [[ "x${IMG_VER}" == 'x' ]]; then
+if [[ "x${IMG_VER}" == 'x' && "x${UNATTENDED}" != 'x' ]]; then
   echo "[!] Unable to deduce the image version, please manually set version"
   read -r -p "[*] Enter version for this image [20180000]: " IMG_VER
 else
+  IMG_VER="${IMG_VER:-20190000}"
   echo "[+] Automatically set version to ${IMG_VER} ."
 fi
 echo -n "[+] Generating Dockerfile..."
 cat << EOF > Dockerfile
 FROM scratch
-MAINTAINER AOSC-dev
+LABEL maintainer="AOSC-dev"
 ADD "${TARBALL_NAME}" /
 CMD ["/bin/bash"]
 ARG CONT_IMG_VER
@@ -25,11 +28,13 @@ ENV CONT_IMG_VER ${IMG_VER}
 EOF
 
 echo " OK!"
-read -n 1 -r -p "[*] Build this image [Y/n]? " BUILD_CHOICE
-echo " "
-if [[ "x${BUILD_CHOICE}" == "xn" ]]; then
-  echo "[+] Aborted."
-  exit 0
+if [[ "x${UNATTENDED}" != 'x' ]]; then
+  read -n 1 -r -p "[*] Build this image [Y/n]? " BUILD_CHOICE
+  echo " "
+  if [[ "x${BUILD_CHOICE}" == "xn" ]]; then
+    echo "[+] Aborted."
+    exit 0
+  fi
 fi
 TMPDIR="$(mktemp -d -p .)"
 cd -- "${TMPDIR}" || (echo "[!] WTF?" && exit 127)
